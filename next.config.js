b5b403237daee3fd46be84c63c9679c6c90bc23d
Rob Stanford <me@robstanford.com>
2023-01-26 10:40:39 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'outreach.stg.pxlp.dev',
+        pathname: '/assets/**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
